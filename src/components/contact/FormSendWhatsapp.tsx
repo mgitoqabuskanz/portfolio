af60b60db4e646e3682cf345fb09eb0c849802f8
Muhammad Gito Qabus Kanz <mgitoqabuskanz@gmail.com>
2025-03-09 16:35:28 +0700

    add type annotation to validateEmail function in FormSendWhatsapp component

--- src/components/contact/FormSendWhatsapp.tsx
+++ src/components/contact/FormSendWhatsapp.tsx
@@ -38,7 +38,7 @@ const FormSendWhatsapp = () => {
     setIsFormValid(isValid);
   };
 
-  const validateEmail = (email) => {
+  const validateEmail = (email: string): boolean => {
     const regex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
     return regex.test(email);
   };
